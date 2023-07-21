ddaf730ca8c69b2ed77a8279b0e0e85477bbcbb3
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-07-21 11:50:53 -0400

    adminMenu View

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def route():
+    return render_template("adminMenu.html")
+
 @app.route("/login")
 def hello_world():
-    return render_template("loginOptions.html")
+    return render_template("adminMenu.html")
 
 @app.route("/login/admin")
 def login_admin():
