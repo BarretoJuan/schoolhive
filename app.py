acf5ab02ecc8cf4ca768c059be5cbacab43643ef
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-07-21 12:02:36 -0400

    routes modify

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def route():
-    return render_template("adminMenu.html")
+    return redirect(url_for("login"))
 
 @app.route("/login")
-def hello_world():
-    return render_template("adminMenu.html")
+def login():
+    return render_template("loginOptions.html")
 
 @app.route("/login/admin")
 def login_admin():
@@ -21,3 +21,11 @@ def login_student():
 @app.route("/login/professor")
 def login_professor():
     return render_template("loginProfessor.html")
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("admin_menu"))
+
+@app.route("/admin/menu")
+def admin_menu():
+    return render_template("adminMenu.html")
