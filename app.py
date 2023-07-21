9df1fe431d8edee108fa169341cc62a62c874cf6
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-07-21 14:50:03 -0400

    templates  creation

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ def route():
 
 @app.route("/login")
 def login():
-    return render_template("loginOptions.html")
+    return render_template("/auth/loginOptions.html")
 
 @app.route("/login/admin")
 def login_admin():
-    return render_template("loginAdmin.html")
+    return render_template("/auth/loginAdmin.html")
 
 @app.route("/login/student")
 def login_student():
-    return render_template("loginStudent.html")
+    return render_template("/auth/loginStudent.html")
 
 @app.route("/login/professor")
 def login_professor():
-    return render_template("loginProfessor.html")
+    return render_template("/auth/loginProfessor.html")
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("admin_menu"))
+    return redirect(url_for("admin_dashboard"))
 
 @app.route("/admin/menu")
-def admin_menu():
-    return render_template("adminMenu.html")
+def admin_dashboard():
+    return render_template("admin/adminDashboard.html")
