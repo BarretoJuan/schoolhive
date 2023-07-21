d1895d47d2255d90a671bd564f629b57700c4f7b
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-07-21 00:53:38 -0400

    login template views

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/login")
 def hello_world():
     return render_template("loginOptions.html")
+
+@app.route("/login/admin")
+def login_admin():
+    return render_template("loginAdmin.html")
+
+@app.route("/login/student")
+def login_student():
+    return render_template("loginStudent.html")
+
+@app.route("/login/professor")
+def login_professor():
+    return render_template("loginProfessor.html")
