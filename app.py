19a6bd4d019a1c353abce7076a3d2240da9ce991
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-07-22 14:54:46 -0400

    student and professor profile edit, routes declaration

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
+#LOGIN ROUTES
 @app.route("/")
 def route():
     return redirect(url_for("login"))
@@ -22,6 +24,9 @@ def login_student():
 def login_professor():
     return render_template("/auth/loginProfessor.html")
 
+
+
+#ADMIN ROUTES
 @app.route("/admin")
 def admin():
     return redirect(url_for("admin_dashboard"))
@@ -34,6 +39,31 @@ def admin_dashboard():
 def admin_profile():
     return render_template("admin/adminProfileEdit.html")
 
-@app.route("/admin/profile")
-def test():
-    return render_template("admin/adminProfileEdit.html")
+
+
+#PROFESSOR ROUTES
+@app.route("/professor")
+def professor():
+    return redirect(url_for("professor_dashboard"))
+
+@app.route("/professor/menu")
+def professor_dashboard():
+    return render_template("professor/professorDashboard.html")
+
+@app.route("/professor/profile")
+def professor_profile():
+    return render_template("professor/professorProfileEdit.html")
+
+
+#STUDENT ROUTES
+@app.route("/student")
+def student():
+    return redirect(url_for("student_dashboard"))
+
+@app.route("/student/menu")
+def student_dashboard():
+    return render_template("student/studentDashboard.html")
+
+@app.route("/student/profile")
+def student_profile():
+    return render_template("student/studentProfileEdit.html")
